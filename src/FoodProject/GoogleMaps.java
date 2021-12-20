a09c5bb5b932500e8b5bd73f2484b76bc0515c67
whelfjhte79 <whelfjhte79@naver.com>
2021-12-21 07:02:55 +0900

    Create find good restaurant search project

--- src/FoodProject/GoogleMaps.java
+++ src/FoodProject/GoogleMaps.java
@@ -21,20 +21,20 @@ public class GoogleMaps {
 	String fileName = "image.jpg";
 	public void setURL(String urlName) {
 		try {
-			imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+ URLEncoder.encode(urlName, "UTF-8") +"&zoom=19&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + URLEncoder.encode(urlName, "UTF-8") + "&key=AIzaSyDWqc_fxHhHQm1gzOx_OE1IA4vfBGQZW1U";
+			imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+ URLEncoder.encode(urlName, "UTF-8") +"&zoom=19&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + URLEncoder.encode(urlName, "UTF-8") + "&key=KEY";
 		}catch(IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
 	public void setURL(double coordX, double coordY) {
-		imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+coordX+","+coordY+ "&zoom=19&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + coordX + "," + coordY + "&key=AIzaSyDWqc_fxHhHQm1gzOx_OE1IA4vfBGQZW1U";
+		imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+coordX+","+coordY+ "&zoom=19&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + coordX + "," + coordY + "&key=KEY";
 	}
 	public void loadMap() {
 
 		try {
-			//imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+ URLEncoder.encode(urlName, "UTF-8") +"&zoom=18&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + URLEncoder.encode(urlName, "UTF-8") + "&key=AIzaSyDWqc_fxHhHQm1gzOx_OE1IA4vfBGQZW1U";
-			//imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center=37.6598492,126.6656916&zoom=18&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C37.6598492,126.6656916&key=AIzaSyDWqc_fxHhHQm1gzOx_OE1IA4vfBGQZW1U";
+			//imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+ URLEncoder.encode(urlName, "UTF-8") +"&zoom=18&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C" + URLEncoder.encode(urlName, "UTF-8") + "&key=KEY";
+			//imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center=37.6598492,126.6656916&zoom=18&scale=1&size=344x300&maptype=roadmap&markers=color:red%7Clabel:E%7C37.6598492,126.6656916&key=KEY";
 
 			URL url = new URL(imageUrl);
 			InputStream is = url.openStream();
